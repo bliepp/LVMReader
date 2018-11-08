3711e9149c895658ec13a4615d72da1e238a1d69
bliepp <comedyclub333.yt@googlemail.com>
2018-11-08 16:54:24 +0100

    Init

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # LVMReader
-A python reader for LabView's export format (*.lvm)
+A python reader for LabView's export format (.lvm)
+
+# Using the program as standalone
+Make it executable
+```bash
+chmod 755 lvmReader.py
+```
+And then use it as following:
+```bash
+./lvmReader File.lvm [export.file]
+```
+
+# Using as a python3 module
+## Reading data from lvm-file
+```python
+from lvmReader import *
+
+# Read file
+my_lvm = LVMReader(path="Path/to/file.lvm")
+
+# Auto-cleaned list of columns
+my_data = my_lvm.data
+# Auto-cleaned list of rows
+my_transposed_data = my_lvm.transposed
+```
+## Exporting to text file
+```python
+# Change filename and export, autoset to original-filename.dat
+my_lvm.filename = "export.dat"
+# array must be a 2D-array
+LVMReader.export(array, path="", space="\t", end="", transposed=False)
+```
+
+## Transpose a 2D-array list
+```python
+LVMReader.transpose(array)
+```
---
+++ lvmReader.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python3
+#
+# A module to load and export LabView .lvm files
+#
+
+class LVMReader():
+    """
+    Load *.lvm files exported by LabView
+
+    Parameters
+    ----------
+    path: string
+        Location of the file
+
+    Attributes
+    ----------
+    filename: string
+        Filename to export to.
+    data: 2D-array
+        The converted and cleaned up data.
+        Every column is a list inside of the data list.
+    transposed: 2D-array
+        The data attribute transposed.
+        Every row is a list inside of the data list.
+
+    Methods
+    -------
+    transpose(list)
+        Transposes a 2D-array list (e.g. matrix, table, ...)
+    export(path="", space="\t", end="", transposed=False)
+        Exports the data as a text table to the given
+        path with the given file name.
+    """
+
+    def __init__(self, path):
+        self.filename = path.split("/")[-1][:-4] + ".dat"
+        self._raw = list()
+        self.__data = list()
+
+        with open(path) as lvm:
+            self._raw = lvm.readlines()
+
+        self.__clean()
+
+    def __clean(self):
+        # Make table
+        for i in range(len(self._raw)):
+            self._raw[i] = self._raw[i].replace(",", ".")
+            self.__data.append(
+                    self._raw[i].strip().split("\t")
+                )
+
+        # Remove header
+        ## Get last occurence of "End Header"
+        i = len(self.__data) - 1 - self.__data[::-1].index(
+                ["***End_of_Header***"]
+            ) + 2
+        self.__data = self.__data[i:]
+
+        # Convert to floats
+        self.__data = [
+                [ float(x) for x in row ] for row in self.__data
+            ]
+        
+        self.__data = self.transposed
+
+    def transpose(array):
+        # Transpose
+        return list(map(list, zip(*array)))[:]
+
+    def export(array, path="", space="\t", end="", transposed=False):
+        """
+        Template for universal exporting.
+        """
+        # inverse logic!
+        if not transposed: array = LVMReader.transpose(array)
+
+        with open(path, "w") as out:
+            for i in array:
+                out.write(space.join(str(x) for x in i) + end)
+                out.write("\n")
+    
+    @property
+    def data(self):
+        return self.__data[:]
+
+    @property
+    def transposed(self):
+        return LVMReader.transpose(self.__data)
+
+if __name__ == "__main__":
+    import sys
+
+    # get filename
+    if len(sys.argv) > 1:
+        # init lvm file
+        lvm = LVMReader(sys.argv[1])
+        
+        # get out file name
+        if len(sys.argv) > 2:
+            lvm.filename = sys.argv[2]
+        
+        # Export and show data
+        LVMReader.export(lvm.data, lvm.filename)
+        print(lvm.data)
